9394164d11ccb87c77f476c3ee2f3331d5aa7719
wjjoo <yellow10332@gmail.com>
2026-01-07 16:08:55 +0900

    1.0.15

--- vite.config.js
+++ vite.config.js
@@ -8,11 +8,29 @@ export default defineConfig({
   plugins: [vue(), tailwindcss()],
   base: "./",
   build: {
+    lib: {
+      entry: path.resolve(__dirname, 'src/index.js'),
+      name: 'VueLibreEditor',
+      fileName: (format) => `vue-libre-editor.${format}.js`
+    },
+    rollupOptions: {
+      external: ['vue'],
+      output: {
+        globals: {
+          vue: 'Vue'
+        },
+        assetFileNames: (assetInfo) => {
+          if (assetInfo.name === 'style.css') {
+            return 'vue-libre-editor.css';
+          }
+          return assetInfo.name;
+        }
+      }
+    },
     minify: false,
     sourcemap: false,
     cssCodeSplit: false,
     target: "esnext",
-    ssr: false,
   },
   resolve: {
     alias: {
